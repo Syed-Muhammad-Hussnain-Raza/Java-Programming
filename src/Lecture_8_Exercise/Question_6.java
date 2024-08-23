a3010e2bce5167e173b54c1fa4723a854eade762
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-23 22:39:28 +0500

    Function - Infinite loop using do-while loop

---
+++ src/Lecture_8_Exercise/Question_6.java
@@ -0,0 +1,10 @@
+package Lecture_8_Exercise;
+
+public class Question_6 {
+    public static void main(String[] args) {
+    // Write an infinite loop using do while condition
+        do {
+            System.out.println("You have been hacked 😈");
+        } while (true);
+    }
+}
