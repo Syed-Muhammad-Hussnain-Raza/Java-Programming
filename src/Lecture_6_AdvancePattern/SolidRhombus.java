432fe9091457a58f982b5a80cd2fcc15af135770
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-23 20:15:54 +0500

    Solid Rhombus Pattern using Java

---
+++ src/Lecture_6_AdvancePattern/SolidRhombus.java
@@ -0,0 +1,21 @@
+package Lecture_6_AdvancePattern;
+
+public class SolidRhombus {
+    public static void main(String[] args) {
+        int n = 7;
+
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+
+            // stars
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(" *");
+            }
+
+            System.out.println();
+        }
+    }
+}
