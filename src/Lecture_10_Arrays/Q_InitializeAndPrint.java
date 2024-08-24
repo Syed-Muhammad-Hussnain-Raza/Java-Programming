172a61db1f345130186809be9e2399779f8b5401
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-24 15:23:47 +0500

    Project file

---
+++ src/Lecture_10_Arrays/Q_InitializeAndPrint.java
@@ -0,0 +1,26 @@
+public class Lecture_10_Arrays {
+    // Arrays in Java
+    public static void main(String[] args) {
+    // Initializing array
+        // Method# 1
+        int size = 5;
+        int [] marksArr1 = new int[size]; // new int[size]
+
+        for (int i = 0; i < marksArr1.length; i++) {
+            marksArr1[i] = (i + 1) * 2;
+        }
+
+        // Method# 2
+        int [] marksArr2 = {1, 2, 3, 4, 5};
+
+        // Printing array
+        // for loop
+        for (int i = 0; i < marksArr1.length; i++) {
+            System.out.println("marksArr1[" + i + "]" + " = " + marksArr1[i]);
+        }
+        // for each loop
+        for (int num : marksArr2) {
+            System.out.println(num);
+        }
+    }
+}
