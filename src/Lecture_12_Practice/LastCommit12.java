59a019970c1ef497775a32e4aa55a8dedc6742e4
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-27 22:11:19 +0500

    Code updated

--- src/Lecture_12_Practice/LastCommit12.java
+++ src/Lecture_12_Practice/LastCommit12.java
@@ -1,5 +1,6 @@
 public class LastCommit12 {
    public static void main(String[] args) {
       System.out.println("'Hi, I am Syed Muhammad Hussnain Raza'");
+      System.out.println("This code has no mean but to made the consistency");
    }
 }
