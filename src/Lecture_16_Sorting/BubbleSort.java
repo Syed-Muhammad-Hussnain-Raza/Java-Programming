6e24e9d6a05287af3c68ec0d2ac42c5eea91b1a4
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-30 06:06:00 +0500

    Bubble sorting technique

---
+++ src/Lecture_16_Sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+package Lecture_16_Sorting;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        // an array with random numbers
+        int[] array = {4, 5, 10, 6, 2, 9, 0, 3, 1, 7, 8};
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
