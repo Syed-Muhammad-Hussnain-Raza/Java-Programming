d02eb335fab17204a22e1c6d51a6a3250d3a72ea
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-30 21:12:42 +0500

    Print number using recursion

---
+++ src/Lecture_17_Recursion/Q1_PrintNumbers.java
@@ -0,0 +1,24 @@
+package Lecture_17_Recursion;
+
+public class Q1_PrintNumbers {
+    public static void main(String[] args) {
+        printAscendingNumbers(1);
+        printDescendingNumbers(5);
+    }
+
+    public static void printAscendingNumbers(int num) {
+        if (num > 5) {
+            return;
+        }
+        System.out.println(num);
+        printAscendingNumbers(num + 1);
+    }
+
+    public static void printDescendingNumbers(int num) {
+        if (num <= 0) {
+            return;
+        }
+        System.out.println(num);
+        printDescendingNumbers(num - 1);
+    }
+}
