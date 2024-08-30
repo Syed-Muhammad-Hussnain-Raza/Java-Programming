b19a53cc97b0c0a43ebd62f01c640e96700a7690
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-30 11:14:21 +0500

    Insertion Sort Algorithm using Java

--- src/Lecture_16_Sorting/InsertionSort.java
+++ src/Lecture_16_Sorting/InsertionSort.java
@@ -4,20 +4,28 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] array = { 4, 5, 10, 6, 2, 9, 0, 3, 1, 7, 8 };
 
-        for (int i = 0; i < array.length; i++) {
-            int current = array[i];
+        insertionSort(array);
+        System.out.print("Sorted array: [ ");
+        for ( int num : array) {
+            System.out.print(num + ", ");
+        }
+        System.out.print("]");
+    }
+
+    // Insertion Sort
+    public static void insertionSort(int[] array) {
+        // Outer loop started here
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i]; // i = 5 | current = 9
             int j = i - 1;
-            // loop started
-            while (j >= 0 && current < array[j]) {
+            // inner loop started here
+            while (j >= 0 && array[j] > current ) {
                 array[j + 1] = array[j];
                 j-- ;
             }
 
             // placement
             array[j + 1] = current ;
-        }
-        for ( int num : array) {
-            System.out.print(num + " ");
         }
     }
 }
