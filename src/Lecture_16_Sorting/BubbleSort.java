6a99bf751429e85b2ef49880a80a375f6856e423
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-30 06:17:08 +0500

    Updated Bubble-Sort code

--- src/Lecture_16_Sorting/BubbleSort.java
+++ src/Lecture_16_Sorting/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
         int[] array = {4, 5, 10, 6, 2, 9, 0, 3, 1, 7, 8};
 
         for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
+            for (int j = 0; j < (array.length - 1) - i; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
@@ -15,8 +15,8 @@ public class BubbleSort {
             }
         }
 
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+        for (int num : array) {
+            System.out.print( num + " " );
         }
     }
 }
