9ada7063d843381147c697bbbca2e61cf71a1fe8
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-30 21:51:39 +0500

    Calculate sum for n numbers

---
+++ src/Lecture_17_Recursion/Q2_CalculateSum.java
@@ -0,0 +1,16 @@
+package Lecture_17_Recursion;
+
+public class Q2_CalculateSum {
+    public static void main(String[] args) {
+        printSum(1, 4, 0);
+    }
+
+    public static void printSum(int value, int end, int sum ) {
+        if (value > end) {
+            System.out.println("Sum: " + sum);
+            return;
+        }
+        sum += value;
+        printSum(value + 1, end, sum );
+    }
+}
