cb305334baf6bf5d31b32b53bd84e26a1d0c12d6
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-31 21:51:12 +0500

    Factorial using recursion

---
+++ src/Lecture_17_Recursion/Q3_Factorial.java
@@ -0,0 +1,23 @@
+package Lecture_17_Recursion;
+
+import java.util.Scanner;
+
+public class Q3_Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        if ( num < 0 ) {
+            System.out.println("Factorial of negative number is not possible");
+            return;
+        }
+        System.out.println("Factorial of " + num +" is: "+ + factorial(num));
+    }
+
+    public static int factorial(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
