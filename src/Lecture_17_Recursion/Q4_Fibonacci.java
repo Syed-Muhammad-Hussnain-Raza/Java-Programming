5ed4bf8a3d90da85c624343b7ded217741df4db8
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-09-02 17:59:17 +0500

    Update file for fibonacci series, now print full series

--- src/Lecture_17_Recursion/Q4_Fibonacci.java
+++ src/Lecture_17_Recursion/Q4_Fibonacci.java
@@ -3,15 +3,28 @@ package Lecture_17_Recursion;
 import java.util.Scanner;
 
 public class Q4_Fibonacci {
-    public static int fibonacci(int n) {
-        if (n <= 1) return n;
-        return fibonacci(n - 1) + fibonacci(n - 2);
+
+    public static void printFibonacci(int a, int b, int n) {
+        if (n <= 0) {
+            return;
+        }
+
+        int c = a + b;
+        System.out.print(c + " ");
+        printFibonacci(b, c, n-1);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number : ");
         int n = sc.nextInt();
-        System.out.println("Fibonacci of " + n + " is " + fibonacci(n));
+
+        int a = 0;
+        int b = 1;
+
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+
+        printFibonacci(a, b, n - 2);
     }
 }
