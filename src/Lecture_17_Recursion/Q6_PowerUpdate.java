d592811d9f4cb523e11664a2c8cd1ae2a614c85d
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-09-02 18:42:30 +0500

    Calculate power having stack height equals to log n

---
+++ src/Lecture_17_Recursion/Q6_PowerUpdate.java
@@ -0,0 +1,37 @@
+package Lecture_17_Recursion;
+
+import java.util.Scanner;
+
+public class Q6_PowerUpdate {
+    public static int calculatePower(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        if (x == 0) {
+            return 0;
+        }
+
+        if (n % 2 == 0) {
+            return calculatePower(x, n/2) * calculatePower(x, n/2);
+        } else  {
+            return calculatePower(x, n/2) * calculatePower(x, n/2) * x;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base: ");
+        int base = sc.nextInt();
+
+        System.out.print("Enter a positive exponent: ");
+        int exponent = sc.nextInt();
+
+        if (exponent < 0) {
+            System.out.println("We not support negative number in exponent yet");
+            return;
+        }
+
+        System.out.println(calculatePower(base, exponent));
+    }
+}
--- src/Lecture_17_Recursion/Q7_ReverseString.java
+++ src/Lecture_17_Recursion/Q7_ReverseString.java
@@ -2,7 +2,7 @@ package Lecture_17_Recursion;
 
 import java.util.Scanner;
 
-public class Q5_ReverseString {
+public class Q6_ReverseString {
     public static String reverseString(String str) {
         if (str.isEmpty()) return str;
         return reverseString(str.substring(1)) + str.charAt(0);
