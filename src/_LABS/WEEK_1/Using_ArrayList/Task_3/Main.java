2d7f1204f3879a3f9856e8931cb6519b651b3876
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-09-04 21:47:25 +0500

    Here linear search apply on task 2 list

--- src/_LABS/WEEK_1/Using_ArrayList/Task_3/Main.java
+++ src/_LABS/WEEK_1/Using_ArrayList/Task_3/Main.java
@@ -1,23 +1,38 @@
 package _LABS.WEEK_1.Using_ArrayList.Task_3;
 
+import _LABS.WEEK_1.Using_ArrayList.Task_2.MyArrayList;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    ArrayList list = new ArrayList(Arrays.asList(1, 2, 3, 4, 5));
+    ArrayList<Integer> newList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    MyArrayList myArrayList = new MyArrayList(newList);
 
-    // Linear search to find element in array list
-    public void linearSearch(int val) {
-        int index = list.indexOf(val);
-        if (index != -1) {
-            System.out.println("Element found at index " + index);
-        } else {
-            System.out.println("Element not found");
+    // Linear search method
+    public int linearSearch(int value) {
+        for (int i = 0; i < myArrayList.list.size(); i++) {
+            if (value == myArrayList.list.get(i)) {
+                return i;
+            }
         }
+        return -1;
     }
 
     public static void main(String[] args) {
         Main m = new Main();
-        m.linearSearch(1);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number to find in array list: ");
+        int value = sc.nextInt();
+
+        int index = m.linearSearch(value);
+        if (index == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index " + index);
+        }
     }
 }
