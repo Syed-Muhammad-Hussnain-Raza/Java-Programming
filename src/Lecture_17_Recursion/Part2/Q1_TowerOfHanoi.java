ed4f64c9d971e44951036c1cd57c111884d29bdc
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-09-07 21:02:47 +0500

    Tower of Hanoi using recursion

---
+++ src/Lecture_17_Recursion/Part2/Q1_TowerOfHanoi.java
@@ -0,0 +1,25 @@
+package Lecture_17_Recursion.Part2;
+
+import java.util.Scanner;
+
+public class Q1_TowerOfHanoi {
+    public static void towerOfHanoi(int n, String src, String helper, String dest) {
+        if (n == 1) {
+            System.out.println("Transfer disk " + n + " from "+ src + " to " + dest);
+            return;
+        }
+
+        towerOfHanoi(n - 1, src, dest, helper);
+        System.out.println("Transfer disk " + n + " from "+ src + " to " + dest);
+        towerOfHanoi(n - 1, helper, src, dest);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of disks: ");
+        int n = sc.nextInt();
+
+        towerOfHanoi(n, "S","H", "D");
+    }
+}
