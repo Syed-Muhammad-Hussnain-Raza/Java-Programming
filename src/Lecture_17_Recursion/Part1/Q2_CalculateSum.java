d706ca2fcb2805e6e7ca1c58b9a52c153f2c83d5
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-09-12 10:39:58 +0500

    Update: Positive changes are made in it

--- src/Lecture_17_Recursion/Part1/Q2_CalculateSum.java
+++ src/Lecture_17_Recursion/Part1/Q2_CalculateSum.java
@@ -1,16 +1,31 @@
 package Lecture_17_Recursion.Part1;
 
 public class Q2_CalculateSum {
-    public static void main(String[] args) {
-        printSum(1, 4, 0);
-    }
 
-    public static void printSum(int value, int end, int sum ) {
+    int sum = 0;
+
+    /**
+     * This function will calculate the sum between two integer numbers
+     * @param value is the value to be added in sum until end reach.
+     * @param end is the value on which our function will terminate.
+     */
+    public void printSum(int value, int end ) {
         if (value > end) {
             System.out.println("Sum: " + sum);
             return;
         }
         sum += value;
-        printSum(value + 1, end, sum );
+        printSum(value + 1, end );
     }
+
+    /**
+     * Main/Test function, This function will run first when run button is presses.
+     * @param args is the of String type pass to main by default
+     */
+    public static void main(String[] args) {
+        Q2_CalculateSum calc = new Q2_CalculateSum();
+
+        calc.printSum(1, 4);
+    }
+
 }
