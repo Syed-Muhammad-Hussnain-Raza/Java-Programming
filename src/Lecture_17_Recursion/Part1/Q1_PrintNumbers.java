61fb460f08b70e7490e0ad95aaee19cb0982c727
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-09-12 10:32:51 +0500

    Update: Positive changes are made in it

--- src/Lecture_17_Recursion/Part1/Q1_PrintNumbers.java
+++ src/Lecture_17_Recursion/Part1/Q1_PrintNumbers.java
@@ -1,24 +1,35 @@
 package Lecture_17_Recursion.Part1;
 
 public class Q1_PrintNumbers {
-    public static void main(String[] args) {
-        printAscendingNumbers(1);
-        printDescendingNumbers(5);
-    }
-
+    /**
+     * Recursive function will print numbers in ascending order from n to 10.
+     * @param num is the number from which the order start to meet 10.
+     */
     public static void printAscendingNumbers(int num) {
-        if (num > 5) {
+        if (num > 10) {
+            System.out.println();
             return;
         }
-        System.out.println(num);
+        System.out.print(num + " ");
         printAscendingNumbers(num + 1);
     }
 
+    /**
+     * Recursive function will print numbers in descending order from n to 1.
+     * @param num is the number from which the order start to meet 1.
+     */
     public static void printDescendingNumbers(int num) {
         if (num <= 0) {
+            System.out.println();
             return;
         }
-        System.out.println(num);
+
+        System.out.print(num + " ");
         printDescendingNumbers(num - 1);
+    }
+
+    public static void main(String[] args) {
+        printAscendingNumbers(1);
+        printDescendingNumbers(10);
     }
 }
