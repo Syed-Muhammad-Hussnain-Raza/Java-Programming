be8f583a86cb9079a40a23cfeee4c372db355318
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-09-12 10:45:19 +0500

    Update: Changes are made in it like positioning of functions and comments

--- src/Lecture_17_Recursion/Part1/Q3_Factorial.java
+++ src/Lecture_17_Recursion/Part1/Q3_Factorial.java
@@ -3,6 +3,23 @@ package Lecture_17_Recursion.Part1;
 import java.util.Scanner;
 
 public class Q3_Factorial {
+
+    /**
+     * Recursive function to calculate factorial of given positive integer number.
+     * @param n is the number whose factorial is to be calculated.
+     * @return will be calculated factorial of given number.
+     */
+    public static int factorial(int n) {
+        // base case
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+
+        // recursive call to function
+        return n * factorial(n - 1);
+    }
+
+    // Main Function to test our methods and functions.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number : ");
@@ -14,10 +31,4 @@ public class Q3_Factorial {
         System.out.println("Factorial of " + num +" is: "+ + factorial(num));
     }
 
-    public static int factorial(int n) {
-        if (n == 1 || n == 0) {
-            return 1;
-        }
-        return n * factorial(n - 1);
-    }
 }
