1cb4876841add3b8cd7ce5bcc3ab804e00a681e9
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-09-12 11:03:57 +0500

    Update: Changes are made in it like comments

--- src/Lecture_17_Recursion/Part1/Q5_Power.java
+++ src/Lecture_17_Recursion/Part1/Q5_Power.java
@@ -3,16 +3,23 @@ package Lecture_17_Recursion.Part1;
 import java.util.Scanner;
 
 public class Q5_Power {
-    // Recursive function
+    /**
+     * This function will calculate power for given exponent to the base.
+     * @param x is base here.
+     * @param n is exponent of base here
+     * @return calculation of base to the power exponent will be return.
+     */
     public static int calculatePower(int x, int n) {
+        // base case 1:
         if (n == 0) {
             return 1;
         }
-
+        // base case 2:
         if (x == 0) {
             return 0;
         }
 
+        // recursive call to function
         return x * calculatePower(x, n-1);
     }
 
