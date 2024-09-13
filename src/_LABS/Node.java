61b6ab0f85670f85ad0d1a29293feabb9df3a154
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-09-13 20:18:58 +0500

    Node Class for single linked list

---
+++ src/_LABS/Node.java
@@ -0,0 +1,12 @@
+package _LABS;
+
+public class Node {
+    int data;  // Value stored in the node
+    Node next; // Reference to the next node
+
+    // Constructor to create a new node
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
