f913f8aa8739e784ce4b7c10fb38c94bf92e060f
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-09-21 23:01:28 +0500

    Node of single linked list without tail pointer

---
+++ src/_Theory/SingleNode.java
@@ -0,0 +1,12 @@
+package _Theory;
+
+public class SingleNode {
+    Object data;
+    SingleNode next;
+
+    // Constructor
+    public SingleNode(Object data) {
+        this.data = data;
+        this.next = null;
+    }
+}
