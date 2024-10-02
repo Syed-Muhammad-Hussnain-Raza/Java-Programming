20460892f38066c6481906ea64f188dcc1a6cf8f
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-10-02 16:42:27 +0500

    Exception class for stack overflow

---
+++ src/_LABS/WEEK_5/Activity_1/StackOverflowException.java
@@ -0,0 +1,8 @@
+package Activity_1;
+
+public class StackOverflowException extends RuntimeException {
+    // our constructor
+    public StackOverflowException(String message) {
+        super(message);
+    }
+}
