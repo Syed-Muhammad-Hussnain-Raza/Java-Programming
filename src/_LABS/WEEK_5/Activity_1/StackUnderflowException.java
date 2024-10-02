b73d12d44b8ef8c3c694a87038c262cfd3f14823
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-10-02 16:42:58 +0500

    Stack underflow exception

---
+++ src/_LABS/WEEK_5/Activity_1/StackUnderflowException.java
@@ -0,0 +1,8 @@
+package Activity_1;
+
+public class StackUnderflowException extends RuntimeException {
+    // our constructor
+    public StackUnderflowException(String message) {
+        super(message);
+    }
+}
