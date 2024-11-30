923d72af4e29b57ad6fac49c275236ee01d35f8a
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-11-30 21:34:10 +0500

    Exception Class

---
+++ src/_LABS/WEEK_5/Graded_Tasks/InvalidPostfixException.java
@@ -0,0 +1,8 @@
+package Graded_Tasks;
+
+public class InvalidPostfixException extends RuntimeException{
+    // Constructor
+    public InvalidPostfixException(String message) {
+        super(message);
+    }
+}
